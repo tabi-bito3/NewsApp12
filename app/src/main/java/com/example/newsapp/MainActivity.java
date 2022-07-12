5ff0cfbdd8a8790376649668c3baa4471991d306
tabi-bito3 <bhavyaardeshna2301@gmail.com>
2022-07-12 21:25:40 +0530

    Update

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -8,14 +8,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
     private FirebaseAuth mAuth;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+
         setSupportActionBar(mainToolbar);
         try {
             Objects.requireNonNull(getSupportActionBar()).setTitle("Campus News");
@@ -33,14 +49,11 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception ex) {
             ex.printStackTrace();
         }
-
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
         FirebaseUser user = mAuth.getCurrentUser();
         if (user == null) {
             sendToLogin();
@@ -65,13 +78,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendToLogin() {
-        Intent intent= new Intent(MainActivity.this,LoginActivity.class);
+        Intent intent= new Intent(MainActivity.this,RegisterActivity.class);
         startActivity(intent);
         finish();
     }
 
     private void logOut() {
-        mAuth.signOut();
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                mAuth.signOut();
+                Toast.makeText(getApplicationContext(), "Logout successful", Toast.LENGTH_SHORT).show();
+                finish();
+                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
         sendToLogin();
     }
 
--- app/src/main/java/com/example/newsapp/RegisterActivity.java
+++ app/src/main/java/com/example/newsapp/RegisterActivity.java
@@ -1,14 +1,131 @@
 package com.example.newsapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    Button gButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        gButton = (Button) findViewById(R.id.google_login);
+
+        // Initialize firebase auth
+        mAuth = FirebaseAuth.getInstance();
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("562224797374-d850td2kmcei3ni71cce6hgnpfbsuknq.apps.googleusercontent.com")
+                .requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        gButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (user != null) {
+            // User is signed in
+            navigateToMainActivity();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1000) {
+            Task <GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            if(task.isSuccessful()) {
+
+                Toast.makeText(getApplicationContext(), "Google sign in successful", Toast.LENGTH_SHORT).show();
+                try {
+                    // Initialize sign in account
+                    GoogleSignInAccount googleSignInAccount=task
+                            .getResult(ApiException.class);
+                    // Check condition
+                    if(googleSignInAccount!=null)
+                    {
+                        // When sign in account is not equal to null
+                        // Initialize auth credential
+                        AuthCredential authCredential= GoogleAuthProvider
+                                .getCredential(googleSignInAccount.getIdToken()
+                                        ,null);
+                        // Check credential
+                        mAuth.signInWithCredential(authCredential)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        // Check condition
+                                        if(task.isSuccessful())
+                                        {
+                                            // When task is successful
+                                            navigateToMainActivity();
+                                            // Display Toast
+                                            Toast.makeText(getApplicationContext(), "Firebase authentication successful", Toast.LENGTH_SHORT).show();
+                                        }
+                                        else
+                                        {
+                                            // When task is unsuccessful
+                                            Toast.makeText(getApplicationContext(), "Authentication Failed :"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+
+                    }
+                } catch (ApiException e) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void signIn() {
+        Intent intent = gsc.getSignInIntent();
+        startActivityForResult(intent, 1000);
+
+    }
+
+    private void navigateToMainActivity() {
+        finish();
+        Intent intent= new Intent(RegisterActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
