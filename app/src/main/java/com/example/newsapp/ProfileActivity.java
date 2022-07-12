90d3d2c426e3554dbe6f30a092e17b9670ed8d09
tabi-bito3 <bhavyaardeshna2301@gmail.com>
2022-07-13 00:22:34 +0530

    Update

--- app/src/main/java/com/example/newsapp/ProfileActivity.java
+++ app/src/main/java/com/example/newsapp/ProfileActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
